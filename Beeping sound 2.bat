b2dfa94e90819ad59eee1feb7675062aaf2219dd
FinalRuze <132386390+FinalRuze@users.noreply.github.com>
2023-05-10 17:48:03 -0400

    Delete Beeping sound 2.bat

--- Beeping sound 2.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-:loop
-PowerShell -Command "Add-Type -AssemblyName PresentationCore; [System.Media.SystemSounds]::Beep.Play(); $null = [System.Windows.MessageBox]::Show('Beep!', '', 'OK', 'None', 'DefaultButton1', 'MessageBoxIcon.Information', 'SystemModal')"
-goto loop
